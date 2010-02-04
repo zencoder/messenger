83db7a80bb8d4e9db0593e6ff9f0dc76c9639803
Brandon Arbini <brandon@sevenwire.com>
2010-02-04 01:01:54 -0800

    Monkey patching xmpp4r-simple to allow a custom host.

--- lib/messenger/jabber.rb
+++ lib/messenger/jabber.rb
@@ -18,3 +18,49 @@ module Messenger
   end
 
 end
+
+
+# MONKEY PATCHING
+#
+# xmpp4r-simple does not allow you to specify the jabber host, but we need to do that for Google Apps.
+
+module Jabber
+
+  class Simple
+
+    def initialize(jid, password, host=nil, status=nil, status_message="Available")
+      @jid = jid
+      @password = password
+      @host = host
+      @disconnected = false
+      status(status, status_message)
+      start_deferred_delivery_thread
+    end
+
+    def connect!
+      raise ConnectionError, "Connections are disabled - use Jabber::Simple::force_connect() to reconnect." if @disconnected
+      # Pre-connect
+      @connect_mutex ||= Mutex.new
+
+      # don't try to connect if another thread is already connecting.
+      return if @connect_mutex.locked?
+
+      @connect_mutex.lock
+      disconnect!(false) if connected?
+
+      # Connect
+      jid = JID.new(@jid)
+      my_client = Client.new(@jid)
+      my_client.connect(@host)
+      my_client.auth(@password)
+      self.client = my_client
+
+      # Post-connect
+      register_default_callbacks
+      status(@presence, @status_message)
+      @connect_mutex.unlock
+    end
+
+  end
+
+end
