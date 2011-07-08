98103ccf0122111e707a690683e6d1672358afd8
Christopher Warren <christopher.warren@gmail.com>
2011-07-08 16:52:38 -0700

    no longer passing empty basic auth headers if user/pass aren't set in the URL

--- test/messenger/web_test.rb
+++ test/messenger/web_test.rb
@@ -11,14 +11,14 @@ class Messenger::WebTest < Test::Unit::TestCase
     end
 
     should "post a successful message" do
-      HTTParty.expects(:send).with(:post, "http://example.com", :body => '{ "key": "value" }', :headers => { "Content-Type" => "application/json" }, :basic_auth => {:username => nil,:password => nil}).returns(@success_response)
+      HTTParty.expects(:send).with(:post, "http://example.com", :body => '{ "key": "value" }', :headers => { "Content-Type" => "application/json" }).returns(@success_response)
       result = Messenger::Web.deliver("http://example.com", '{ "key": "value" }', :headers => { "Content-Type" => "application/json" })
       assert result.success?
       assert_equal @success_response, result.response
     end
 
     should "post a failed message" do
-      HTTParty.expects(:send).with(:post, "http://example.com", :body => '{ "key": "value" }', :headers => { "Content-Type" => "application/json" }, :basic_auth => {:username => nil,:password => nil}).returns(@failure_response)
+      HTTParty.expects(:send).with(:post, "http://example.com", :body => '{ "key": "value" }', :headers => { "Content-Type" => "application/json" }).returns(@failure_response)
       result = Messenger::Web.deliver("http://example.com", '{ "key": "value" }', :headers => { "Content-Type" => "application/json" })
       assert_equal false, result.success?
       assert_equal @failure_response, result.response
