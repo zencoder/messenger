8bfc78b37b4d71189c05077e5dabe10d84f763f5
Matthew McClure <matt@mmcc.io>
2015-03-02 16:10:08 -0800

    added Slack integration and tests

---
+++ lib/messenger/slack.rb
@@ -0,0 +1,68 @@
+require 'httparty'
+require 'json'
+
+class Messenger::Slack
+
+  def self.valid_url?(url)
+    !!matcher(url)
+  rescue NoMethodError
+    false
+  end
+
+  # URL format:
+  #     slack://displayname@hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX/#room
+  # Slack supports the notion of attachments, so the messages can be a little fancier. If body is a string, that
+  # string will simply be sent to the room as text. `body` can also be an array of attachments, or a single attachment.
+  # Attachment reference: https://api.slack.com/docs/attachments
+  def self.deliver(url, body, options={})
+    raise Messenger::URLError, "The URL provided is invalid" unless valid_url?(url)
+    display_name, base_url, key_one, key_two, secret, channel = matcher(url)
+    options[:headers] ||= {}
+    response = HTTParty.post(
+      "https://#{base_url}/#{key_one}/#{key_two}/#{secret}",
+      :headers => { "Content-Type" => "application/json"}.merge(options[:headers]),
+      :body => build_message(channel, display_name, body)
+    )
+    Messenger::Result.new(success?(response), response)
+  end
+
+  def self.obfuscate(url)
+    raise Messenger::URLError, "The URL provided is invalid" unless valid_url?(url)
+    display_name, base_url, key_one, key_two, secret, channel = matcher(url)
+    "slack://#{display_name}@#{base_url}/T********/B********/************************/#{channel}"
+  end
+
+
+private
+
+  def self.build_message(channel, display_name, body)
+    msg = { channel: channel, username: display_name }
+
+    return msg.merge({ text: body }) if body.is_a?(String)
+
+    if body.is_a?(Array)
+      msg['attachments'] = body
+    end
+
+    if body.is_a?(Hash)
+      msg['attachments'] = []
+      msg['attachments'].push(body)
+    end
+
+    msg.to_json
+  end
+
+  def self.matcher(url)
+    url.match(/slack:\/\/(.*)@(hooks.slack.com\/services)\/([T].*?)\/([B].*?)\/(.*)\/([#@].*)/)[1,6]
+  end
+
+  def self.success?(response)
+    case response.code
+    when 200, 201
+      true
+    else
+      false
+    end
+  end
+
+end
---
+++ test/messenger/slack_test.rb
@@ -0,0 +1,69 @@
+require "test_helper"
+
+class Messenger::SlackTest < Test::Unit::TestCase
+
+  context "Slack notification" do
+    setup do
+      @success_response = stub(:code => 200)
+      @failure_response = stub(:code => 500)
+    end
+
+    should "post a successful message as a string" do
+      HTTParty.expects(:post).with("https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX", :body => { channel: '#room', username: 'displayname', text: 'hello world!' }, :headers => { "Content-Type" => "application/json"}).returns(@success_response)
+      result = Messenger::Slack.deliver("slack://displayname@hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX/#room", 'hello world!')
+      assert result.success?
+      assert_equal @success_response, result.response
+    end
+
+    should "post a successful message as an object" do
+      HTTParty.expects(:post).with("https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX", :body => { channel: '#room', username: 'displayname', attachments: [{ fallback: 'hello world!', color: 'good' }] }.to_json, :headers => { "Content-Type" => "application/json"}).returns(@success_response)
+      result = Messenger::Slack.deliver("slack://displayname@hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX/#room", { fallback: 'hello world!', color: 'good' })
+      assert result.success?
+      assert_equal @success_response, result.response
+    end
+
+    should "post a successful message as an array" do
+      HTTParty.expects(:post).with("https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX", :body => { channel: '#room', username: 'displayname', attachments: [{ fallback: 'hello world!', color: 'good' }, { fallback: 'meh', color: 'warning' }] }.to_json, :headers => { "Content-Type" => "application/json"}).returns(@success_response)
+      result = Messenger::Slack.deliver("slack://displayname@hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX/#room", [{ fallback: 'hello world!', color: 'good' }, { fallback: 'meh', color: 'warning' }])
+      assert result.success?
+      assert_equal @success_response, result.response
+    end
+
+    should "post a failed message" do
+      HTTParty.expects(:post).with("https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX", :body => { channel: '#room', username: 'displayname', text: 'hello world!' }, :headers => { "Content-Type" => "application/json"}).returns(@failure_response)
+      result = Messenger::Slack.deliver("slack://displayname@hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX/#room", 'hello world!')
+      assert_equal false, result.success?
+      assert_equal @failure_response, result.response
+    end
+
+    should "raise when sending to an invalid URL" do
+      assert_raises Messenger::URLError do
+        Messenger::Slack.deliver("slack://hooks.slack.com/blah/blah", 'content')
+      end
+    end
+
+    should "obfuscate the URL" do
+      assert_equal "slack://displayname@hooks.slack.com/services/T********/B********/************************/#room", Messenger::Slack.obfuscate("slack://displayname@hooks.slack.com/services/T1234567/B12334567/asdfasdfasdfasdfasdf/#room")
+    end
+
+    should "raise when obfuscating an invalid URL" do
+      assert_raises Messenger::URLError do
+        Messenger::Slack.obfuscate("slack://hooks.slack.com/blah/blah")
+      end
+    end
+  end
+
+  context "Slack URL validation" do
+    should "return true for good URLs" do
+      assert_equal true, Messenger::Slack.valid_url?("slack://displayname@hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX/#room")
+    end
+
+    should "return false for bad URLs" do
+      assert_equal false, Messenger::Campfire.valid_url?("slack://!")
+      assert_equal false, Messenger::Campfire.valid_url?("slack://user@hooks.slack.com")
+      assert_equal false, Messenger::Campfire.valid_url?("slack://hooks.slack.com")
+      assert_equal false, Messenger::Campfire.valid_url?("slack://user@hooks.slack.com/asdfasdf/asdfasdf/asdfasdf")
+      assert_equal false, Messenger::Campfire.valid_url?("slack://user@hooks.slack.com/asdfasdf")
+    end
+  end
+end
